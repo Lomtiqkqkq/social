aab2549807227dc0583c7c897c3417fe03792549
Lomti_qkqkq <mokrenko51@gmail.com>
2024-09-05 16:18:04 +0300

    fix: Postgres config fix Update postgres config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import initPostgres from './db/init.postgres';
+import InitPostgres from './db/init.postgres';
 dotenv.config()
 async function bootstrap() {
     const app = express()
     const port = process.env.PORT || 3000
-    // initPostgres()
+    await InitPostgres.sync().then(()=>{
+        console.log(`db connection started`);
+    })
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
     app.listen(port, () => {
